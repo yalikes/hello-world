11c96ea8e9f6da1cc5aaa51a8e260d66ff2675b8
paohui <2054453988@qq.com>
2019-12-05 14:57:33 +0800

    add makefile practice file

---
+++ make/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(int argc,char** argv)
+{
+	extern int universe;
+	cout<<"the answer of universe is: "<<universe<<endl;
+}
---
+++ make/src/data.cpp
@@ -0,0 +1,1 @@
+int universe=42;
